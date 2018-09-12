95b9adec2fc15e0eb3bf064b47750c697def7b3e
Yana <yana_dontsova@mail.ru>
2018-09-12 21:40:08 +0300

    qq

--- task5.js
+++ task5.js
@@ -2,11 +2,44 @@ const path = require('path');
 const fs = require('fs');
 
 
+if (process.argv.length < 3) {
+    console.log("Недостаточно аргументов");
+    process.exit();
+}
+if (process.argv.length > 3) {
+    console.log("много аргументов");
+    process.exit();
+}
+
 const DIR_PATH = process.argv[2];
 const NEW_DIRECTORY = DIR_PATH + '\\' + path.basename(DIR_PATH);
 let prefix = "";
 const sum = fs.createWriteStream('summary.js');
 let copyright = "";
+
+let readAndCopyDirectory = function (dir, prefix) {
+    fs.readdir(dir, (err, files) => {
+        if(err) {
+            console.error("ошибка чтения файлов в директории " + dir);
+        } else {
+            files.forEach(function(element) {
+                let new_unit = dir + '\\' + element;
+                if(fs.statSync(new_unit).isDirectory()) {
+                    readAndCopyDirectory(new_unit, prefix + element + '/');
+                } else {
+                    sum.write('console.log(\'' + prefix + element + '\');\n');
+                    let new_file = `${NEW_DIRECTORY}\\${path.basename(new_unit)}`;
+                    let logger = fs.createWriteStream(new_file);
+                    fs.readFile(new_unit, (err, data) => {
+                        if(err) console.error("ошибка при копировании файла")
+                        else logger.write(copyright + '\n\n-------\n' + data + '\n--------\n\n' + copyright);
+                    });
+
+                }
+            }, this);
+        }
+    });
+}
 
 
 let createDir = function (callback) {
@@ -20,11 +53,11 @@ let createDir = function (callback) {
                 console.log(`${eventType} - ${filename}`);
             });
         }
-        else console.log("директорий уже существует!");
+        else console.log("директорий уже существует");
     });
-    // получение copyright
+
     fs.readFile("config.json", (err, data) => {
-        if (err) console.error("Пошибка при чтении файла")
+        if (err) console.error("ошибка при чтении файла")
         else {
             copyright = JSON.parse(data).copyright;
         }
