f3686edb0502bf2bfeb4fc3d2885b5f89377c10d
Yana <yana_dontsova@mail.ru>
2018-09-12 21:26:54 +0300

    metod create

--- task5.js
+++ task5.js
@@ -1,3 +1,35 @@
 const path = require('path');
 const fs = require('fs');
 
+
+const DIR_PATH = process.argv[2];
+const NEW_DIRECTORY = DIR_PATH + '\\' + path.basename(DIR_PATH);
+let prefix = "";
+const sum = fs.createWriteStream('summary.js');
+let copyright = "";
+
+
+let createDir = function (callback) {
+    // создание нового директория
+    fs.access(NEW_DIRECTORY, (err) => {
+        if(err && err.code == 'ENOENT') {
+            fs.mkdir(NEW_DIRECTORY, (err) => {
+                if (err) console.error("ошибка при создании директории");
+            });
+            fs.watch(NEW_DIRECTORY, (eventType, filename) => {
+                console.log(`${eventType} - ${filename}`);
+            });
+        }
+        else console.log("директорий уже существует!");
+    });
+    // получение copyright
+    fs.readFile("config.json", (err, data) => {
+        if (err) console.error("Пошибка при чтении файла")
+        else {
+            copyright = JSON.parse(data).copyright;
+        }
+    });
+    callback();
+}
+
+
